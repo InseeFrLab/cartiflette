4dbde59f3b8bac0152c039c06ec37b0ed274b80a
linogaliana <linogaliana@yahoo.fr>
2022-07-27 14:32:30 +0000

    pylint

--- _download_pb.py
+++ _download_pb.py
@@ -1,5 +1,8 @@
-import requests
+"""
+Download files with nice progress bars
+"""
 import os
+import requests
 from tqdm import tqdm
 
 
@@ -12,11 +15,12 @@ def _download_pb(url: str, fname: str, total: int = None):
     Arguments:
         url {str} -- URL for the source file
         fname {str} -- Destination where data will be written
+        total {int} -- Filesize. Optional argument, recommended to let the default value.
     """
 
     try:
         proxies = {"http": os.environ["http_proxy"], "https": os.environ["https_proxy"]}
-    except:
+    except KeyError:
         proxies = {"http": "", "https": ""}
 
     resp = requests.get(url, proxies=proxies, stream=True)
@@ -36,8 +40,18 @@ def _download_pb(url: str, fname: str, total: int = None):
             obj.update(size)
 
 
-def _download_pb_ftp(ftp, url, fname):
-    with open(fname, 'wb') as fd:
+def _download_pb_ftp(ftp, url: str, fname: str):
+    """Useful function to get request with a progress bar
+
+    Borrowed from
+    https://gist.github.com/yanqd0/c13ed29e29432e3cf3e7c38467f42f51
+
+    Arguments:
+        url {str} -- URL for the source file
+        fname {str} -- Destination where data will be written
+        ftp -- ftplib object
+    """
+    with open(fname, 'wb') as file:
         total = ftp.size(url)
         with tqdm(total=total,
                 unit_scale=True,
@@ -45,7 +59,7 @@ def _download_pb_ftp(ftp, url, fname):
                 miniters=1,
                 leave=False
                 ) as pbar:
-            def cb(data):
+            def dowload_write(data):
                 pbar.update(len(data))
-                fd.write(data)
-            ftp.retrbinary(f'RETR {url}', cb)
+                file.write(data)
+            ftp.retrbinary(f'RETR {url}', dowload_write)
