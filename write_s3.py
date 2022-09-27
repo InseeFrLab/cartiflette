b39fcf85e58e2219689f5c45c0a48c46c6be8ad4
linogaliana <linogaliana@yahoo.fr>
2022-09-27 09:14:54 +0000

    Write S3 tests

---
+++ write_s3.py
@@ -0,0 +1,29 @@
+import os
+
+os.chdir("cartogether")
+
+import s3
+
+s3.write_shapefile_s3_all(
+    level="COMMUNE",
+    shapefile_format="geojson",
+    decoupage="region",
+    year=2022)
+
+s3.write_shapefile_s3_all(
+    level="COMMUNE",
+    shapefile_format="geojson",
+    decoupage="region",
+    year=2021)
+
+s3.write_shapefile_s3_all(
+    level="COMMUNE",
+    shapefile_format="GPKG",
+    decoupage="region",
+    year=2019)
+
+s3.write_shapefile_s3_all(
+    level="COMMUNE",
+    shapefile_format="shp",
+    decoupage="region",
+    year=2020)
