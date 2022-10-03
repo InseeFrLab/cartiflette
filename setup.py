efe16d312de66526a77c6cf25230f0ddad5fccd7
linogaliana <linogaliana@yahoo.fr>
2022-10-03 12:51:10 +0000

    Packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_data={'': ['README.md', 'sources.yaml']},
+    include_package_data=True,
+    zip_safe=False)
