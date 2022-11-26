cdd7ed58f1250316c7287d6dcf022204fb6aa16e
lgaliana <lino.galiana@insee.fr>
2022-11-26 15:14:35 +0000

    version fonctionnelle par territoire

--- cartiflette/download/__init__.py
+++ cartiflette/download/__init__.py
@@ -1,3 +1,7 @@
-from .dev import get_vectorfile_ign, get_administrative_level_available_ign, get_BV
+from .dev import get_vectorfile_ign, get_administrative_level_available_ign,\
+    store_ign_vectorfile, get_BV
 
-__all__ = ["get_vectorfile_ign", "get_administrative_level_available_ign", "get_BV"]
+__all__ = [
+    "get_vectorfile_ign", "get_administrative_level_available_ign",
+    "store_ign_vectorfile",
+    "get_BV"]
