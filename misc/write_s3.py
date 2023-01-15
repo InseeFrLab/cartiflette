4032e99ea7a94746a378aef16d1d48b85c801f7d
lgaliana <lino.galiana@insee.fr>
2023-01-15 13:12:30 +0000

    changelevel

--- misc/write_s3.py
+++ misc/write_s3.py
@@ -35,58 +35,6 @@ s3.production_cartiflette(
 )
 
 
-for format, couple_decoupage_level, year, epsg in itertools.product(
-    formats, croisement_decoupage_level_flat, years, crs_list
-    ):
-    lev = couple_decoupage_level[0]
-    decoup = couple_decoupage_level[1]
-    print(80*'==' + "\n" \
-        f"level={lev}\nvectorfile_format={format}\n" \
-        f"decoupage={decoup}\nyear={year}\n" \
-        f"crs={epsg}"
-        )
-    s3.write_vectorfile_s3_all(
-        level=lev,
-        vectorfile_format=format,
-        decoupage=decoup,
-        year=year,
-        crs=epsg,
-        provider="IGN",
-        source=source)
-
-
-
-#formats = ["geoparquet", "shp", "gpkg", "geojson"]
-formats = ["geojson", "topojson"]
-#decoupage = ["region", "departement"]
-decoupage = ["departement"]
-years = [y for y in range(2021, 2023)]
-for format, decoup, year in itertools.product(
-    formats, decoupage, years
-    ):
-    s3.write_vectorfile_s3_custom_arrondissement(
-            vectorfile_format="geojson",
-            decoupage="departement",
-            crs=4326,
-            year=year)
-
-
-#formats = ["geoparquet", "shp", "gpkg", "geojson"]
-formats = "geojson"
-decoupage = ["france_entiere"]
-level = ["COMMUNE", "ARRONDISSEMENT"]
-years = [y for y in range(2021, 2023)]
-for format, decoup, lev, year in itertools.product(
-    formats, decoupage, level, years
-    ):
-    s3.write_vectorfile_s3_all(
-        level=lev,
-        vectorfile_format=format,
-        decoupage=decoup,
-        year=year)
-
-
-
 
 # OLD --------------
 
