dd5d3d7bdd8ccd6ad5bbebc179631f60a2a7284b
linogaliana <lino.galiana@insee.fr>
2023-09-06 08:12:55 +0000

    import dans le module

--- cartiflette/s3/s3.py
+++ cartiflette/s3/s3.py
@@ -17,6 +17,7 @@ from cartiflette.utils import (
     create_format_standardized,
     create_format_driver,
     download_pb,
+    create_path_bucket,
     official_epsg_codes,
 )
 
@@ -128,57 +129,6 @@ def create_url_s3(
     print(url)
 
     return url
-
-
-def create_path_bucket(
-    bucket: str = BUCKET,
-    path_within_bucket: str = PATH_WITHIN_BUCKET,
-    provider: str = "IGN",
-    source: str = "EXPRESS-COG-TERRITOIRE",
-    vectorfile_format: str = "geojson",
-    borders: str = "COMMUNE",
-    filter_by: str = "region",
-    year: typing.Union[str, int, float] = "2022",
-    value: typing.Union[str, int, float] = "28",
-    crs: typing.Union[str, int, float] = 2154,
-) -> str:
-    """
-    This function creates a file path for a vector file within a specified bucket.
-
-    Parameters:
-    bucket (str): The name of the bucket where the file will be stored.
-    path_within_bucket (str): The path within the bucket where the file will be stored.
-    vectorfile_format (str): The format of the vector file,
-        can be "geojson", "topojson", "gpkg" or "shp". Default is "geojson".
-    borders (str): The administrative borders of the tiles within the vector file.
-        Can be any administrative
-        borders provided by IGN, e.g. "COMMUNE", "DEPARTEMENT" or "REGION". Default is "COMMUNE".
-    filter_by (str): The administrative borders (supra to 'borders') that will be
-        used to cut the vector file in pieces when writing to S3. For instance, if
-        borders is "DEPARTEMENT", filter_by can be "REGION" or "FRANCE_ENTIERE".
-        Default is "region".
-    year (str): The year of the vector file. Default is "2022".
-    value (str): The value of the vector file. Default is "28".
-    crs (int): The coordinate reference system of the vector file. Default is 2154.
-
-    Returns:
-    str: The complete file path for the vector file that will be used to read
-    or write when interacting with S3 storage
-    """
-
-    write_path = f"{bucket}/{path_within_bucket}"
-    write_path = f"{write_path}/{year=}"
-    write_path = f"{write_path}/administrative_level={borders}"
-    write_path = f"{write_path}/{crs=}"
-    write_path = f"{write_path}/{filter_by}={value}/{vectorfile_format=}"
-    write_path = f"{write_path}/{provider=}/{source=}"
-    write_path = f"{write_path}/raw.{vectorfile_format}"
-    write_path = write_path.replace("'", "")
-
-    if vectorfile_format == "shp":
-        write_path = write_path.rsplit("/", maxsplit=1)[0] + "/"
-
-    return write_path
 
 
 # DOWNLOAD FROM S3 --------------------------
