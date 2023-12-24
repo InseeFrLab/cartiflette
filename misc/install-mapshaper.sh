4e274f6ca8cf7960ba2b5ec2d8ab23f2d15c277f
linogaliana <lino.galiana@insee.fr>
2023-12-24 09:20:42 +0000

    checkout

--- misc/install-mapshaper.sh
+++ misc/install-mapshaper.sh
@@ -3,8 +3,8 @@ sudo apt install npm nodejs -y
 sudo apt-get install libmagic-dev -y
 
 git clone https://github.com/mbloch/mapshaper.git --single-branch
-git checkout ec6e7a40bc875ee6d113045e5ee86f47abf57bc7
 cd mapshaper
+git checkout ec6e7a40bc875ee6d113045e5ee86f47abf57bc7
 yes | npm install       # install dependencies
 yes yes | npm run build     # bundle source code files
 sudo npm link          # (optional) add global symlinks so scripts are available systemwide
