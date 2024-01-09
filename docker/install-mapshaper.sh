e07e27de815af374238258f4d4eb6a25621588ae
linogaliana <lino.galiana@insee.fr>
2024-01-09 15:11:59 +0000

    sudo

--- docker/install-mapshaper.sh
+++ docker/install-mapshaper.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sudo apt-get update -y
-sudo apt install npm nodejs -y
-sudo apt-get install libmagic-dev -y
+apt-get update -y
+apt install npm nodejs -y
+apt-get install libmagic-dev -y
 
 git clone https://github.com/mbloch/mapshaper.git --single-branch
 cd mapshaper
 git checkout ec6e7a40bc875ee6d113045e5ee86f47abf57bc7
 yes | npm install       # install dependencies
 yes yes | npm run build     # bundle source code files
-sudo npm link          # (optional) add global symlinks so scripts are available systemwide
+npm link          # (optional) add global symlinks so scripts are available systemwide
