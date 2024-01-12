e3cc436d300449961db4039be151c2e2fbe219f4
Lino Galiana <lino.galiana@insee.fr>
2024-01-12 17:54:55 +0000

    logger

--- misc/argo-pipeline/src/crossproduct.py
+++ misc/argo-pipeline/src/crossproduct.py
@@ -1,38 +1,35 @@
-from cartiflette.pipeline import crossproduct_parameters_production
+import argparse
 import json
+import logging
+from cartiflette.pipeline import crossproduct_parameters_production
 
-# parameters
-formats = ["topojson", "geojson"]
-years = [2022]
-crs_list = [4326]
-sources = ["EXPRESS-COG-CARTO-TERRITOIRE"]
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-croisement_decoupage_level = {
-    # structure -> niveau geo: [niveau decoupage macro],
-    "COMMUNE": [
-        "BASSIN_VIE", "ZONE_EMPLOI", "UNITE_URBAINE", "AIRE_ATTRACTION_VILLES",  # zonages d'études
-        "DEPARTEMENT", "REGION",  # zonages administratifs
-        "TERRITOIRE", "FRANCE_ENTIERE", "FRANCE_ENTIERE_DROM_RAPPROCHES"],
-    "DEPARTEMENT": ["REGION", "TERRITOIRE", "FRANCE_ENTIERE", "FRANCE_ENTIERE_DROM_RAPPROCHES"],
-    "REGION": ["TERRITOIRE", "FRANCE_ENTIERE", "FRANCE_ENTIERE_DROM_RAPPROCHES"],
-    "BASSIN_VIE": ["TERRITOIRE", "FRANCE_ENTIERE"],
-    "ZONE_EMPLOI": ["TERRITOIRE", "FRANCE_ENTIERE"],
-    "UNITE_URBAINE": ["TERRITOIRE", "FRANCE_ENTIERE"],
-    "AIRE_ATTRACTION_VILLES": ["TERRITOIRE", "FRANCE_ENTIERE"]
-}
-
+parser = argparse.ArgumentParser(description='Crossproduct Script')
+parser.add_argument('--restrictfield', type=str, default=None, help='Field to restrict level-polygons')
 
 def main():
+    args = parser.parse_args()
+    
     tempdf = crossproduct_parameters_production(
         croisement_filter_by_borders=croisement_decoupage_level,
         list_format=formats,
         years=years,
         crs_list=crs_list,
         sources=sources, simplifications=[0, 50])
+    
     tempdf.columns = tempdf.columns.str.replace("_", "-")
+
+    # Apply filtering if restrictfield is provided
+    if args.restrictfield:
+        tempdf = tempdf.loc[tempdf['level-polygons'] == args.restrictfield]
+
     output = tempdf.to_json(orient="records")
     parsed = json.loads(output)
-    print(json.dumps(parsed))
+    
+    # Use logger to output the JSON
+    logging.info(json.dumps(parsed))
 
 if __name__ == "__main__":
     main()
