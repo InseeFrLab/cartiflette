2877f41434ff951a49ae4ffbe75040c37d7461f8
Lino Galiana <lino.galiana@insee.fr>
2024-01-12 18:01:48 +0000

    restriction to one field

--- misc/argo-pipeline/src/crossproduct.py
+++ misc/argo-pipeline/src/crossproduct.py
@@ -1,24 +1,40 @@
-import argparse
 import json
-import logging
+import argparse
 from cartiflette.pipeline import crossproduct_parameters_production
-
-# Configure logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 parser = argparse.ArgumentParser(description='Crossproduct Script')
 parser.add_argument('--restrictfield', type=str, default=None, help='Field to restrict level-polygons')
 
+
+# parameters
+formats = ["topojson", "geojson"]
+years = [2022]
+crs_list = [4326]
+sources = ["EXPRESS-COG-CARTO-TERRITOIRE"]
+
+croisement_decoupage_level = {
+    # structure -> niveau geo: [niveau decoupage macro],
+    "COMMUNE": [
+        "BASSIN_VIE", "ZONE_EMPLOI", "UNITE_URBAINE", "AIRE_ATTRACTION_VILLES",  # zonages d'études
+        "DEPARTEMENT", "REGION",  # zonages administratifs
+        "TERRITOIRE", "FRANCE_ENTIERE", "FRANCE_ENTIERE_DROM_RAPPROCHES"],
+    "DEPARTEMENT": ["REGION", "TERRITOIRE", "FRANCE_ENTIERE", "FRANCE_ENTIERE_DROM_RAPPROCHES"],
+    "REGION": ["TERRITOIRE", "FRANCE_ENTIERE", "FRANCE_ENTIERE_DROM_RAPPROCHES"],
+    "BASSIN_VIE": ["TERRITOIRE", "FRANCE_ENTIERE"],
+    "ZONE_EMPLOI": ["TERRITOIRE", "FRANCE_ENTIERE"],
+    "UNITE_URBAINE": ["TERRITOIRE", "FRANCE_ENTIERE"],
+    "AIRE_ATTRACTION_VILLES": ["TERRITOIRE", "FRANCE_ENTIERE"]
+}
+
+args = parser.parse_args()
+
 def main():
-    args = parser.parse_args()
-    
     tempdf = crossproduct_parameters_production(
         croisement_filter_by_borders=croisement_decoupage_level,
         list_format=formats,
         years=years,
         crs_list=crs_list,
         sources=sources, simplifications=[0, 50])
-    
     tempdf.columns = tempdf.columns.str.replace("_", "-")
 
     # Apply filtering if restrictfield is provided
@@ -27,9 +43,8 @@ def main():
 
     output = tempdf.to_json(orient="records")
     parsed = json.loads(output)
-    
-    # Use logger to output the JSON
-    logging.info(json.dumps(parsed))
+    print(json.dumps(parsed))
+
 
 if __name__ == "__main__":
     main()
