6d69ace8221596c02b48e284d263f8bc51eb7188
thomas.grandjean <thomas.grandjean@developpement-durable.gouv.fr>
2024-08-27 17:33:00 +0200

    Update __init__.py
    * remove unused prepare_local_directory_mapshaper

--- cartiflette/pipeline/__init__.py
+++ cartiflette/pipeline/__init__.py
@@ -3,7 +3,6 @@ from .cross_product_parameters import (
     crossproduct_parameters_production,
 )
 
-from .prepare_mapshaper import prepare_local_directory_mapshaper
 from .mapshaper_split_from_s3 import (
     mapshaperize_split_from_s3,
     mapshaperize_merge_split_from_s3,
@@ -12,7 +11,6 @@ from .mapshaper_split_from_s3 import (
 __all__ = [
     "restructure_nested_dict_borders",
     "crossproduct_parameters_production",
-    "prepare_local_directory_mapshaper",
     "mapshaperize_split_from_s3",
     "mapshaperize_merge_split_from_s3",
 ]
