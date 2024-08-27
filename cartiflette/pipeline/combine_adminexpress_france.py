c79f21e26a2a55b21fc747c204cddb6d177539f9
thomas.grandjean <thomas.grandjean@developpement-durable.gouv.fr>
2024-08-27 17:32:25 +0200

    Update combine_adminexpress_france.py
    * add docstring
    * refacto for looping over years
    * cleanup temporary dirs

--- cartiflette/pipeline/combine_adminexpress_france.py
+++ cartiflette/pipeline/combine_adminexpress_france.py
@@ -1,25 +1,57 @@
 import os
 import re
+import shutil
 import subprocess
+from typing import Union
 
+import s3fs
 
 from cartiflette.config import FS, BUCKET, PATH_WITHIN_BUCKET
 from cartiflette.s3 import BaseGISDataset
 
 
-COMPILED_YEAR = re.compile("year=([0-9]{4})")
 COMPILED_TERRITORY = re.compile("territory=([a-z]*)/", flags=re.IGNORECASE)
 
 
 def combine_adminexpress_territory(
-    intermediate_dir="temp",
-    bucket=BUCKET,
-    path_within_bucket=PATH_WITHIN_BUCKET,
-    fs=FS,
-):
+    year: Union[str, int],
+    intermediate_dir: str = "temp",
+    bucket: str = BUCKET,
+    path_within_bucket: str = PATH_WITHIN_BUCKET,
+    fs: s3fs.S3FileSystem = FS,
+) -> str:
+    """
+    Merge cities datasets into a single file (full France territory).
+
+    All files are retrieved from S3, projected to mercator coordinates, then
+    merged using mapshaper. Every computation is done on the disk, inside
+    a temporary file.
+
+    Parameters
+    ----------
+    year : Union[str, int]
+        Desired vintage
+    intermediate_dir : str, optional
+        Temporary dir to process files. The default is "temp".
+    bucket : str, optional
+        Storage bucket on S3 FileSystem. The default is BUCKET.
+    path_within_bucket : str, optional
+        Path within S3 bucket used for storage. The default is
+        PATH_WITHIN_BUCKET.
+    fs : s3fs.FyleSystem, optional
+        S3 file system used for storage of raw data. The default is FS.
+
+    Returns
+    -------
+    output_path : str
+        Path of merged dataset. Should be
+        f"{intermediate_dir}/{year}/preprocessed_combined/raw.geojson"
+
+    """
+
     config = {
-        "bucket": BUCKET,
-        "path_within_bucket": PATH_WITHIN_BUCKET,
+        "bucket": bucket,
+        "path_within_bucket": path_within_bucket,
         "provider": "IGN",
         "dataset_family": "ADMINEXPRESS",
         "source": "EXPRESS-COG-TERRITOIRE",
@@ -29,45 +61,60 @@ def combine_adminexpress_territory(
         "value": "raw",
         "vectorfile_format": "shp",
         "simplification": 0,
-        "intermediate_dir": intermediate_dir,
     }
+
     path = (
         f"{bucket}/{path_within_bucket}/"
         "provider=IGN/dataset_family=ADMINEXPRESS/"
-        "source=EXPRESS-COG-TERRITOIRE/**/COMMUNE.*"
+        "source=EXPRESS-COG-TERRITOIRE/"
+        f"year={year}/"
+        "**/COMMUNE.*"
     )
 
     format_intermediate = "geojson"
 
     communes_paths = fs.glob(path)
     dirs = {os.path.dirname(x) for x in communes_paths}
-    years = {y for x in dirs for y in COMPILED_YEAR.findall(x)}
     territories = {t for x in dirs for t in COMPILED_TERRITORY.findall(x)}
 
-    for year in years:
+    try:
         for territory in territories:
             with BaseGISDataset(
-                year=year, territory=territory, **config
+                fs=fs,
+                intermediate_dir=f"{intermediate_dir}/{year}",
+                year=year,
+                territory=territory,
+                **config,
             ) as dset:
                 dset.to_mercator()
 
-        output_path = f"{intermediate_dir}/preprocessed_combined/raw.{format_intermediate}"
+        output_path = (
+            f"{intermediate_dir}/{year}/preprocessed_combined/"
+            f"raw.{format_intermediate}"
+        )
 
         subprocess.run(
             (
-                f"mapshaper -i {intermediate_dir}/preprocessed/*.geojson combine-files name='COMMUNE' "
+                f"mapshaper -i {intermediate_dir}/{year}/preprocessed/*.geojson"
+                " combine-files name='COMMUNE' "
                 f"-proj EPSG:4326 "
                 f"-merge-layers "
                 f"-o {output_path} "
-                f'format={format_intermediate} extension=".{format_intermediate}" singles'
+                f"format={format_intermediate} "
+                'extension=".{format_intermediate}" singles'
             ),
             shell=True,
             check=True,
         )
-        raise Exception("Stopping here !")
+    except Exception:
+        raise
+    finally:
+        shutil.rmtree(
+            f"{intermediate_dir}/{year}/preprocessed", ignore_errors=True
+        )
 
     return output_path
 
 
-if __name__ == "__main__":
-    combine_adminexpress_territory()
+# if __name__ == "__main__":
+#     combine_adminexpress_territory(2022)
