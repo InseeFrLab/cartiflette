6fb085d1c161df0b456f39f73c130ad3f82e3b22
Thomas Grandjean <tgrandje@gmail.com>
2024-09-01 12:24:13 +0200

    Update __init__.py

--- cartiflette/mapshaper/__init__.py
+++ cartiflette/mapshaper/__init__.py
@@ -4,10 +4,12 @@ Handling spatial data with mapshaper behind the stage
 from .mapshaperize import mapshaperize_split, mapshaperize_split_merge
 from .mapshaper_convert_mercator import mapshaper_convert_mercator
 from .mapshaper_closer import mapshaper_bring_closer
+from .mapshaper_wrangling import mapshaper_enrich
 
 __all__ = [
     "mapshaperize_split",
     "mapshaperize_split_merge",
     "mapshaper_convert_mercator",
     "mapshaper_bring_closer",
+    "mapshaper_enrich",
 ]
