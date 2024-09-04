ef094f0da9cf64f06223ba9fb0479c7488def080
thomas.grandjean <tgrandje@gmail.com>
2024-09-04 18:22:42 +0200

    Move Dataset class to cartiflette level

--- cartiflette/s3/__init__.py
+++ cartiflette/s3/__init__.py
@@ -1,7 +1,4 @@
 from .download_vectorfile import download_vectorfile_url_all
-from .dataset import BaseGISDataset
+from .dataset import BaseGISDataset, Dataset
 
-__all__ = [
-    "download_vectorfile_url_all",
-    "BaseGISDataset",
-]
+__all__ = ["download_vectorfile_url_all", "BaseGISDataset", "Dataset"]
