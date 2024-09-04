ce7d4f714474f5bec6bc4329685475b08a398032
thomas.grandjean <tgrandje@gmail.com>
2024-09-04 18:19:56 +0200

    Add mashaper_split to cartiflette.mapshaper level

--- cartiflette/mapshaper/__init__.py
+++ cartiflette/mapshaper/__init__.py
@@ -4,7 +4,7 @@ Handling spatial data with mapshaper behind the stage
 from .mapshaperize import mapshaperize_split, mapshaperize_split_merge
 from .mapshaper_convert_mercator import mapshaper_convert_mercator
 from .mapshaper_closer import mapshaper_bring_closer
-from .mapshaper_wrangling import mapshaper_enrich
+from .mapshaper_wrangling import mapshaper_enrich, mapshaper_split
 
 __all__ = [
     "mapshaperize_split",
@@ -12,4 +12,5 @@ __all__ = [
     "mapshaper_convert_mercator",
     "mapshaper_bring_closer",
     "mapshaper_enrich",
+    "mapshaper_split",
 ]
