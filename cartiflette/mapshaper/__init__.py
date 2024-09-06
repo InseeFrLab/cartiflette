1687135f4a718f91f920bd49cb71039548671ba5
thomas.grandjean <tgrandje@gmail.com>
2024-09-06 15:26:34 +0200

    remove mapshaperize_split_merge as function

--- cartiflette/mapshaper/__init__.py
+++ cartiflette/mapshaper/__init__.py
@@ -1,13 +1,11 @@
 """
 Handling spatial data with mapshaper behind the stage
 """
-from .mapshaperize import mapshaperize_split_merge
 from .mapshaper_convert_mercator import mapshaper_convert_mercator
 from .mapshaper_closer import mapshaper_bring_closer
 from .mapshaper_wrangling import mapshaper_enrich, mapshaper_split
 
 __all__ = [
-    "mapshaperize_split_merge",
     "mapshaper_convert_mercator",
     "mapshaper_bring_closer",
     "mapshaper_enrich",
