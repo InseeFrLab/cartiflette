b6b07521c88215f3a73148857f590c62e7558061
thomas.grandjean <tgrandje@gmail.com>
2024-09-14 22:01:24 +0200

    Fix selection of vintage in dev environment

--- argo-pipeline/src/select_downstream_vintage_to_process.py
+++ argo-pipeline/src/select_downstream_vintage_to_process.py
@@ -41,6 +41,20 @@ download_results = args.download_results
 
 download_results = json.loads(download_results)
 
+if os.environ["ENVIRONMENT"] == "dev":
+    logging.warning("dev environment -> force generation of each vintage")
+    json_md5 = f"{BUCKET}/{PATH_WITHIN_BUCKET}/md5.json"
+    with FS.open(json_md5, "r") as f:
+        all_md5 = json.load(f)
+    datasets = all_md5["IGN"]["ADMINEXPRESS"]["EXPRESS-COG-CARTO-TERRITOIRE"]
+    years = list(
+        {
+            year
+            for (_territory, vintaged_datasets) in datasets.items()
+            for year in vintaged_datasets.keys()
+        }
+    )
+
 # Example of download_results
 # {"IGN": {"ADMINEXPRESS": {"EXPRESS-COG-TERRITOIRE": {"guadeloupe": {"2024": {"downloaded": true, "paths": {"COMMUNE": ["projet-cartiflette/test/provider=IGN/dataset_family=ADMINEXPRESS/source=EXPRESS-COG-TERRITOIRE/year=2024/administrative_level=None/crs=5490/origin=raw/vectorfile_format=shp/territory=guadeloupe/simplification=0/COMMUNE.shp"]}}}, "martinique": {"2024": {"downloaded": true, "paths": {"COMMUNE": ["projet-cartiflette/test/provider=IGN/dataset_family=ADMINEXPRESS/source=EXPRESS-COG-TERRITOIRE/year=2024/administrative_level=None/crs=5490/origin=raw/vectorfile_format=shp/territory=martinique/simplification=0/COMMUNE.shp"]}}}, "guyane": {"2024": {"downloaded": true, "paths": {"COMMUNE": ["projet-cartiflette/test/provider=IGN/dataset_family=ADMINEXPRESS/source=EXPRESS-COG-TERRITOIRE/year=2024/administrative_level=None/crs=2972/origin=raw/vectorfile_format=shp/territory=guyane/simplification=0/COMMUNE.shp"]}}}, "reunion": {"2024": {"downloaded": true, "paths": {"COMMUNE": ["projet-cartiflette/test/provider=IGN/dataset_family=ADMINEXPRESS/source=EXPRESS-COG-TERRITOIRE/year=2024/administrative_level=None/crs=2975/origin=raw/vectorfile_format=shp/territory=reunion/simplification=0/COMMUNE.shp"]}}}, "mayotte": {"2024": {"downloaded": true, "paths": {"COMMUNE": ["projet-cartiflette/test/provider=IGN/dataset_family=ADMINEXPRESS/source=EXPRESS-COG-TERRITOIRE/year=2024/administrative_level=None/crs=4326/origin=raw/vectorfile_format=shp/territory=mayotte/simplification=0/COMMUNE.shp"]}}}, "metropole": {"2024": {"downloaded": true, "paths": {"COMMUNE": ["projet-cartiflette/test/provider=IGN/dataset_family=ADMINEXPRESS/source=EXPRESS-COG-TERRITOIRE/year=2024/administrative_level=None/crs=2154/origin=raw/vectorfile_format=shp/territory=metropole/simplification=0/COMMUNE.shp"]}}}}}}, "Insee": {"COG": {"DEPARTEMENT": {"france_entiere": {"2024": {"downloaded": false, "paths": null}}}, "REGION": {"france_entiere": {"2024": {"downloaded": false, "paths": null}}}}, "TAGC": {"APPARTENANCE": {"france_entiere": {"2024": {"downloaded": true, "paths": {"table-appartenance-geo-communes-2024": ["projet-cartiflette/test/provider=Insee/dataset_family=TAGC/source=APPARTENANCE/year=2024/administrative_level=None/crs=None/origin=raw/vectorfile_format=xlsx/territory=france_entiere/simplification=0/table-appartenance-geo-communes-2024.xlsx"]}}}}}}}
 
@@ -63,20 +77,7 @@ finally:
     years_geodata = sorted(list(years_geodata))
 
     if os.environ["ENVIRONMENT"] == "dev":
-        logging.warning("dev environment -> force generation of each dataset")
-        if not years_geodata:
-            # Perform on all years
-            json_md5 = f"{BUCKET}/{PATH_WITHIN_BUCKET}/md5.json"
-            with FS.open(json_md5, "r") as f:
-                all_md5 = json.load(f)
-            datasets = all_md5["IGN"]["ADMINEXPRESS"][
-                "EXPRESS-COG-CARTO-TERRITOIRE"
-            ]
-            years = {
-                year
-                for (_territory, vintaged_datasets) in datasets.items()
-                for year in vintaged_datasets.keys()
-            }
+        years_geodata = years
 
     with open("geodatasets_years.json", "w") as out:
         json.dump(years_geodata, out)
@@ -102,20 +103,7 @@ finally:
     years_metadata = sorted(list(years_metadata))
 
     if os.environ["ENVIRONMENT"] == "dev":
-        logging.warning("dev environment -> force generation of each dataset")
-        if not years_metadata:
-            # Perform on all years
-            json_md5 = f"{BUCKET}/{PATH_WITHIN_BUCKET}/md5.json"
-            with FS.open(json_md5, "r") as f:
-                all_md5 = json.load(f)
-            datasets = all_md5["IGN"]["ADMINEXPRESS"][
-                "EXPRESS-COG-CARTO-TERRITOIRE"
-            ]
-            years = {
-                year
-                for (_territory, vintaged_datasets) in datasets.items()
-                for year in vintaged_datasets.keys()
-            }
+        years_metadata = years
 
     with open("metadata_years.json", "w") as out:
         json.dump(years_metadata, out)
