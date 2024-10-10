7a3f90feef23c6d655fd447deac361c2346cd381
lgaliana <lino.galiana@insee.fr>
2024-10-10 08:25:22 +0000

    Test publish sur pypi

--- cartiflette/__init__.py
+++ cartiflette/__init__.py
@@ -1,5 +1,10 @@
 from cartiflette.api import download_from_cartiflette_inner
 
+print("This is an experimental version of cartiflette published on PyPi.")
+print("To use the latest stable version, you can install it directly from GitHub with the following command:")
+print("pip install git+https://github.com/inseeFrLab/cartiflette.git")
+
+
 carti_download = download_from_cartiflette_inner
 
 __all__ = ["carti_download"]
